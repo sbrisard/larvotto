30e29fec2a6678d58711e3b067d9f58663f9ae4c
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-01-15 10:46:58 +0100

    Docstrings

--- include/larvotto/larvotto.hpp
+++ include/larvotto/larvotto.hpp
@@ -12,12 +12,21 @@
 #include <string>
 
 namespace larvotto {
-DllExport std::string author();
+/** Return the author of the larvotto library. */
+std::string author();
 
-DllExport std::string version();
+/** Return the version of the larvotto library. */
+std::string version();
 
-DllExport int return_one();
+/** Return 1. */
+int return_one();
 
+/**
+ * Convert an iterator to a string.
+ *
+ * The string is delimited by square brackets. The elements are separated by a
+ * comma.
+ */
 template <class Iterator>
 std::string repr(Iterator begin, Iterator end) {
   std::ostringstream stream;
@@ -32,15 +41,24 @@ std::string repr(Iterator begin, Iterator end) {
   return stream.str();
 }
 
+/**
+ * This class defines a simulation box.
+ *
+ * @tparam DIM The number of spatial dimensions
+ */
 template <size_t DIM>
 class SimulationBox {
  public:
+  /** Dimensions of the box. */
   const std::array<double, DIM> L;
 
+  /** Default type for a spatial vector. */
   typedef std::array<double, DIM> Vector;
 
+  /** Create a new instance with specified dimensions. */
   explicit SimulationBox(const Vector L) : L(L){};
 
+  /** Return a representation of this object as a string. */
   [[nodiscard]] std::string repr() const {
     std::ostringstream stream;
     stream << "SimulationBox<" << DIM
@@ -48,6 +66,18 @@ class SimulationBox {
     return stream.str();
   }
 
+  /**
+   * Return the minimum image `r` of the difference `r₀ = y - x`.
+   *
+   * This vector is defined as follows
+   * @code
+   * rᵢ = r₀ᵢ + nᵢ × Lᵢ,
+   * @endcode
+   * where
+   * @code
+   * nᵢ = arg min {|r₀ᵢ + m × Lᵢ|, m = …, -2, -1, 0, 1, 2, …}.
+   * @endcode
+   */
   Vector minimum_image(const Vector x, const Vector y) const {
     Vector r{};
     for (auto i = 0; i < DIM; i++) {
