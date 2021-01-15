fda4d7885613101c44df07dbf0265c4f761db950
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-01-15 08:20:56 +0100

    [SimulationBox] Basic implementation of min img convention

--- include/larvotto/larvotto.hpp
+++ include/larvotto/larvotto.hpp
@@ -6,13 +6,65 @@
 #define DllExport
 #endif
 
+#include <algorithm>
+#include <array>
+#include <sstream>
 #include <string>
 
-namespace larvotto
-{
-  DllExport std::string author();
+namespace larvotto {
+DllExport std::string author();
 
-  DllExport std::string version();
+DllExport std::string version();
 
-  DllExport int return_one();
+DllExport int return_one();
+
+template <class Iterator>
+std::string repr(Iterator begin, Iterator end) {
+  std::ostringstream stream;
+  stream << "[";
+  for (auto i = begin; i < end; i++) {
+    stream << *i;
+    if (i < end - 1) {
+      stream << ",";
+    }
+  }
+  stream << "]";
+  return stream.str();
+}
+
+template <size_t DIM>
+class SimulationBox {
+ public:
+  const std::array<double, DIM> L;
+
+  typedef std::array<double, DIM> Vector;
+
+  explicit SimulationBox(const Vector L) : L(L){};
+
+  [[nodiscard]] std::string repr() const {
+    std::ostringstream stream;
+    stream << "SimulationBox<" << DIM
+           << ">(L=" << larvotto::repr(L.cbegin(), L.cend()) << ")";
+    return stream.str();
+  }
+
+  Vector minimum_image(const Vector x, const Vector y) const {
+    Vector r{};
+    for (auto i = 0; i < DIM; i++) {
+      r[i] = y[i] - x[i];
+      if (2 * r[i] > L[i]) {
+        r[i] -= L[i];
+      }
+      if (2 * r[i] < -L[i]) {
+        r[i] += L[i];
+      }
+    }
+    return r;
+  }
+};
+
+template <size_t DIM>
+std::ostream& operator<<(std::ostream& os, const SimulationBox<DIM> box) {
+  return os << box.repr();
+}
 }  // namespace larvotto
--- tests/test_larvotto.cpp
+++ tests/test_larvotto.cpp
@@ -3,10 +3,36 @@
 #include "catch2/catch.hpp"
 #include "larvotto/larvotto.hpp"
 
+double minimum_image(double x, double y, double L) {
+  int arg_min = -2;
+  double r_min = std::numeric_limits<double>::max();
+  for (int n = -1; n < 2; n++) {
+    const double r = fabs(y - x + n * L);
+    if (r < r_min) {
+      arg_min = n;
+      r_min = r;
+    }
+  }
+  return y - x + arg_min * L;
+}
+
 TEST_CASE("Test case #1") {
   SECTION("Section #1") {
     REQUIRE(larvotto::author() == "S. Brisard");
     REQUIRE(larvotto::version() == "0.1");
     REQUIRE(larvotto::return_one() == 1);
+  }
+}
+
+TEST_CASE("Minimum image (1D)") {
+  larvotto::SimulationBox<1> box({0.9});
+  std::array<double, 10> positions{0.0, 0.1, 0.2, 0.3, 0.4,
+                                   0.5, 0.6, 0.7, 0.8, 0.9};
+  for (auto x : positions) {
+    for (auto y : positions) {
+      auto actual = box.minimum_image({x}, {y})[0];
+      auto expected = minimum_image(x, y, box.L[0]);
+      REQUIRE(actual == expected);
+    }
   }
 }
