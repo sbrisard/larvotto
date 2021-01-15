962a7ed04a9df9157333c1c6e8acb3380eb0be5c
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-01-15 08:18:48 +0100

    [Sphinx] Define fill-column for emacs

--- sphinx/.dir-locals.el
+++ sphinx/.dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((rst-mode . ((compile-command . "sphinx-build -b html . ../docs"))))
+((rst-mode . ((fill-column . 80)
+	      (compile-command . "sphinx-build -b html . ../docs"))))
